81bcefd710984c9234c485d338ad311e991b8f39
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-04-26 11:26:49 -0400

    adding tables

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.SmallIntegerField()
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    bookingdate = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.SmallIntegerField()
+    title = models.CharField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField()
