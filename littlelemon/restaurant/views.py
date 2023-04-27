8ca5ae062ea05ff4e6934e4eda4927af223af940
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-04-27 14:41:55 -0400

    authentication added.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,12 +3,16 @@ from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
 from rest_framework import generics, viewsets
 from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -20,3 +24,8 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]  
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
