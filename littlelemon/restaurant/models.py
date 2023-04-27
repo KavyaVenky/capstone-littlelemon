9a4dd9b309f3a6d5be07b9eb91c78120f53c6724
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-04-26 21:10:51 -0400

    created apis

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    id = models.SmallIntegerField()
+    id = models.SmallIntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField()
     bookingdate = models.DateTimeField()
 
 class Menu(models.Model):
-    id = models.SmallIntegerField()
-    title = models.CharField()
+    id = models.SmallIntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField()
